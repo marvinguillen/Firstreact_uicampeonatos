df8e9547f5dac46ff2041e90a51b8bc36362d667
marvin.a.guillen@gmail.com <marvin.a.guillen@gmail.com>
2021-01-26 17:54:34 -0600

    Third  app  React to show message to use map

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Tweet from './tweet';
 import './App.css'; 
 
 function App(){
-
-  const [IsRed,setRed]=useState(false);
-  const [count,setCount]=useState(0);
-  const[user,setUser]=useState({
-    name:"Elvis",
+ const[count,setCount]=useState(0);
+  const[user,setUsers]=useState([
+    {name:"Elvis",
     age:25,
-    post:['my first post','my lovely summer']
-  });
+    message:'my first post'},
+    {name:"Jonh",
+    age:56,
+    message:'my second post'},
+    {name:"Hugo",
+    age:78,
+    message:'my third post'}
+
+
+  ]);
   
   const increment = () => {
     setCount(count+1);
@@ -18,13 +24,10 @@ function App(){
   return(
    <div className="App">
      <h1>Hello React APP component</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
-    <Tweet name="DEV ED" message="This is random message"/>
-    <Tweet name="LIVERPOL" message="This is random message2"/>
-    <Tweet name="CHELSEA " message="This is random message3"/>
-    <Tweet name="EVERTON" message="This is random message4"/>
-   </div> 
+      {user.map(user =>(
+        <Tweet name ={user.name} message={user.message}></Tweet>
+      ))}
+       </div> 
   );
 }
 export default App;
