7eef2abe1ff4d85a13db2011944e300e7e5bd784
marvin.a.guillen@gmail.com <marvin.a.guillen@gmail.com>
2021-01-30 16:47:22 -0600

    Six app use ES7 to counter button and increment values

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import {Greet} from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
-
+import Message from './components/Message';
+import Counter from './components/Counter';
 function App(){
  const[count,setCount]=useState(0);
   const[user,setUsers]=useState([
@@ -26,9 +27,11 @@ function App(){
   };
   return(
    <div className="App">
- 
+     <Counter/>
+ {/*<Message/>*/}
+
       {/* <Hello />*/}
-      {/* <Welcome />*/}
+      {/* <Welcome />
     <Greet name="Bruce" heroName="Batman">
       <p>
         This is children props
@@ -41,7 +44,7 @@ function App(){
     <Welcome name="Bruce" heroName="Batman"/>
     <Welcome name="Clark" heroName="Superman"/>
     <Welcome name="Diana" heroName="Wonder woman"/>
-    
+    */}
     </div>
   );
 }
